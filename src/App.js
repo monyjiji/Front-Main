ba1f9fd1dd763991c073e5850ca0213985614177
Omnya Hajjaj <omnyloulou03@gmail.com>
2024-06-22 23:05:52 +0200

    init commit

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/home';
+import Error from './pages/Error';
+import PatientAppointments from './pages/Patients/Appointment';
+import PatientProfile from './pages/Patients/Profile';
+import PatientNewAppointment from './pages/Patients/NewApp';
+import PatientSignUp from './pages/Patients/SignUp';
+import DoctorSignUp from './pages/Doctors/Signup'
+import LogIn from './pages/Patients/SignIn';
+import DoctorLogIn from './pages/Doctors/SignIn';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import DocProfile from './pages/Patients/Doctors/DocProfile';
+import Booking from './pages/Patients/Doctors/Booking';
+import DoctorList from './pages/Patients/Doctors/DoctorList';
+import CurrentAppointments from './pages/Doctors/CurrentAppointments';
+import HistoryAppointments from './pages/Doctors/HistoryAppointments';
+import ProfileDoc from './pages/Doctors/ProfileDoc';
+import PatProfile from './pages/Doctors/PatProfile';
+import './App.css';
+function App() {
+  return (
+   
+    <BrowserRouter>
+       <div className="app-container">
+        <Header />
+        <div className="thecontent">
+
+
+       
+            <Routes>
+              <Route path="/" element={<Home />}/>
+              <Route path="/home" element={<Home />}/>
+              <Route path="/patient/appointments" element={<PatientAppointments />}/>
+              <Route path="/patient/profile" element={<PatientProfile />}/>
+              <Route path="/patient/newapp" element={<PatientNewAppointment />}/>
+              <Route path="/patient/DocProfile" element={<DocProfile />}/>
+              <Route path="/patient/Booking" element={<Booking/>}/> 
+              <Route path="/patient/signup" element={<PatientSignUp/>}/>
+              <Route path="/doctor/signup" element={<DoctorSignUp/>}/>
+              <Route path="/login" element={<LogIn />}/>
+              <Route path="/doctor/login" element={<DoctorLogIn/>}/>
+              <Route path="*" element={<Error />}/>
+              <Route path="/patient/Doctorlist" element={<DoctorList/>}/>
+              <Route path="/doctor/appointments" element={<CurrentAppointments/>}/>
+              <Route path="/doctor/historyappointments" element={<HistoryAppointments/>}/>
+              <Route path="/doctor/ProfileDoc" element={<ProfileDoc/>}/>
+              <Route path="/doctor/PatProfile" element={<PatProfile/>}/>
+            </Routes>
+         </div>   
+        <Footer />
+        </div>
+      </BrowserRouter>
+     
+   
+  );
+}
+
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,65 @@
+
+import React from 'react';
+
+
+function Footer() {
+    return (
+     
+            <footer className="text-center text-white" style={{ backgroundColor: '#3f51b5' }}>
+                <div className="footercontainer" >
+                    <section className="mt" >
+                        <div className="row text-center d-flex justify-content-center pt-5 " >
+                            <div className="col-md-2">
+                                <h6 className="text-uppercase font-weight-bold">
+                                    <a href="#!" className="text-white">About us</a>
+                                </h6>
+                            </div>
+                            <div className="col-md-2">
+                                <h6 className="text-uppercase font-weight-bold">
+                                    <a href="#!" className="text-white">Products</a>
+                                </h6>
+                            </div>
+                            <div className="col-md-2">
+                                <h6 className="text-uppercase font-weight-bold">
+                                    <a href="#!" className="text-white">Awards</a>
+                                </h6>
+                            </div>
+                            <div className="col-md-2">
+                                <h6 className="text-uppercase font-weight-bold">
+                                    <a href="#!" className="text-white">Help</a>
+                                </h6>
+                            </div>
+                            <div className="col-md-2">
+                                <h6 className="text-uppercase font-weight-bold">
+                                    <a href="#!" className="text-white">Contact</a>
+                                </h6>
+                            </div>
+                        </div>
+                    </section>
+
+                    <hr className="my-5" />
+
+                    <section className="mb">
+                        <div className="row d-flex justify-content-center">
+                            <div className="col-lg-8">
+                                <p>
+                                Partnering with only the most qualified and experienced medical professionals, we guarantee that you receive top-notch care. Our rigorous vetting process ensures that every doctor on our platform meets the highest standards of medical excellence.
+                                </p>
+                            </div>
+                        </div>
+                    </section>
+
+                   
+                </div>
+
+                
+            </footer>
+       
+    );
+}
+
+     
+   
+  
+  export default Footer;
+  
